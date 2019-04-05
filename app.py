c90124be67020dec8ec566720f7db3c5ed1f0640
haavard@vaage.com <haavard@vaage.com>
2019-04-05 15:12:47 +0200

    Rework flask server to behave same way as tornado

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask 
 from flask import jsonify
 from flask import request
-import requests
 import socket
+import json
 
 
 #!/usr/bin/env python
@@ -15,27 +15,31 @@ racks = [
     {
         "id": 1,
         "location": u"Hovedbygget, Glshuagen",
-        "latitude": u"64.41947",
-        "longditude": u"10.40093", 
+        "latitude": u"63.41947",
+        "longitude": u"10.40093", 
         "parkingspots": 20,
         "available_spots": 15
     },
     {
         "id": 2,
-        "location": u"Torget",
-        "latitude": u"63.43012",
-        "longditude": u"10.39508", 
+        "location": "Torget",
+        "leatitude": u"63.43012",
+        "longitude": u"10.39508", 
         "parkingspots": 30,
         "available_spots": 29
     },
     {
         "id": 3,
         "location": u"Solsiden",
-        "latitude": u"64.43427",
-        "longditude": u"10.41275", 
+        "latitude": u"63.43427",
+        "longitude": u"10.41275", 
         "parkingspots": 10,
         "available_spots": 0
     }
+
+]
+
+reservations = [
 
 ]
 
@@ -47,7 +51,7 @@ def addRack(rack_id):
         "id": rack_id,
         "location": request.json.get('location'),
         "latitude": request.json.get('latitude'),
-        "longditude": request.json.get('longditude'),
+        "longiitude": request.json.get('longditude'),
         "parkingspots": 10,
         "available_spots": 0
     })
@@ -55,13 +59,16 @@ def addRack(rack_id):
     return 'all good'
 
 
-@app.route('/reserve/<int:rack_id>', methods=['PUT'])
-def reserve(rack_id):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('', 5001))
-    s.send(b"reserve rack")
-    print('Sent message')
+@app.route('/reserve', methods=['PUT'])
+def reserve():
+    body = request.json
+    reservations.append(body)
+    return 'Reservation added'
 
+
+@app.route('/reservations', methods=["PUT"])
+def reservation():
+    return jsonify(reservations)
 
 @app.route('/', methods=['GET'])
 def index():
